cece28fab44d765f099fdfb51728e7ca0011324d
hewj <wenjunhe@ctrip.com>
2018-07-17 14:35:10 +0800

    proptypes

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React,{ Component } from 'react';
+import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
     constructor(props) {
@@ -6,17 +7,27 @@ class TodoItem extends Component {
         this.handleClick = this.handleClick.bind(this);
     }
     render() {
-        const { item } = this.props;
+        const { item,test } = this.props;
         return(
             <li 
-                dangerouslySetInnerHTML={{__html:item}}
-                onClick={this.handleClick}></li>
+                // dangerouslySetInnerHTML={{__html:item}}
+                onClick={this.handleClick}>{item} - {test}</li>
         )
     }
     handleClick() {
         const {handleButtonDelete,index} = this.props;
         handleButtonDelete(index);
     }
+}
+
+TodoItem.propTypes = {
+    item: PropTypes.string,
+    handleButtonDelete: PropTypes.func,
+    index: PropTypes.number,
+    test: PropTypes.oneOfType([PropTypes.string,PropTypes.number]).isRequired
+}
+TodoItem.defaultProps = {
+    test: 'test'
 }
 
 export default TodoItem;
