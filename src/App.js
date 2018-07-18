cd516d3bbdd2b102c65a8a1bd604313809c1f1d9
hewj <wenjunhe@ctrip.com>
2018-07-18 17:45:44 +0800

    antd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React,{ Component } from 'react';
+import { Input,Button,List  } from 'antd';
+import 'antd/dist/antd.css';
+
+const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+];
 
 class App extends Component {
     render() {
         return(
-          <div>
-              hello world!
-          </div>
+            <div style={{margin:10}}>
+                <Input placeholder="todo list" style={{width: 300,marginRight:10}} />
+                <Button type="primary">提交</Button>
+                <List
+                    header={<div>Header</div>}
+                    footer={<div>Footer</div>}
+                    bordered
+                    dataSource={data}
+                    renderItem={item => (<List.Item>{item}</List.Item>)}
+                    style={{width:300,marginTop:10}}
+                />
+            </div>
         )
     }
 }
