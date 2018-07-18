bf9e68046eb363d7234cf8b20df670b992b2ffc1
hewj <wenjunhe@ctrip.com>
2018-07-18 17:08:20 +0800

    清空master

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import React,{ Component,Fragment } from 'react';
-import './style.css';
+import React,{ Component } from 'react';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            show: true
-        }
-        this.handleButton = this.handleButton.bind(this);
-    }
     render() {
         return(
-           <Fragment>
-               <div className = {this.state.show ? 'show' : 'hide'}>hello</div>
-               <span className = {this.state.show ? 'showAni' : 'hideAni'}>hello</span>
-               <hr/>
-               <button onClick={this.handleButton}>toggle</button>
-               
-           </Fragment>
+          <div>
+              hello world!
+          </div>
         )
-    }
-    handleButton() {
-        this.setState(()=>({
-            show: !this.state.show
-        }))
     }
 }
 
